6e90be913e70ea7f9d71fa405a4abab340f822f1
yerasylbekenov <77799107+yerasylbekenov@users.noreply.github.com>
2021-06-10 22:10:00 +0600

    Add files via upload

---
+++ DeepestLeaveSum/src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+            public int deepestLeavesSum(TreeNode root) {
+                Queue<TreeNode> q = new LinkedList<>();
+                q.add(root);
+                int ans = 0, qlen = 0;
+                while (q.size() > 0) {
+                    qlen = q.size();
+                    ans = 0;
+                    for (int i = 0; i < qlen; i++) {
+                        TreeNode curr = q.poll();
+                        ans += curr.val;
+                        if (curr.left != null) q.add(curr.left);
+                        if (curr.right != null) q.add(curr.right);
+                    }
+                }
+                return ans;
+            }
+    }
+}
